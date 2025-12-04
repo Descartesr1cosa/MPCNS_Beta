c95876a0048ca466b68d2f75a9b9a64f2776e7ac
Descartesr1cosa <xuke31415@163.com>
2025-12-04 16:50:27 +0800

    调整求解器中输出、边界处理

--- Z2_MHD/MHD_Solver.h
+++ Z2_MHD/MHD_Solver.h
@@ -82,8 +82,6 @@ public:
 
             copy_field();
         }
-        output_.output_plt_field();
-        // return;
 
         while (true)
         {
@@ -123,7 +121,8 @@ public:
             // 5. 更新控制
             control_.Update();
             if (control_.if_outfile)
-                output_.output_plt_field(); // output_field();
+                output_.output_plt_cell_field(output_.var_defaut_plt_name);
+            // output_.output_plt_field(); // output_.output_plt_cell_field(output_.var_defaut_plt_name); // output_field();
             if (control_.if_stop)
             {
                 output_.output_field();
