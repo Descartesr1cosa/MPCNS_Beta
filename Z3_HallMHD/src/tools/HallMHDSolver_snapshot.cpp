21bf46b2164eff62b4ebabfa7c4eb593725c5d53
Descartesr1cosa <xuke31415@163.com>
2025-12-22 21:56:50 +0800

    copy中应用old的范围记录，因为old中没有虚网格，容易数组越界

--- Z3_HallMHD/src/tools/HallMHDSolver_snapshot.cpp
+++ Z3_HallMHD/src/tools/HallMHDSolver_snapshot.cpp
@@ -8,8 +8,8 @@ void HallMHDSolver::copy_field()
             auto &U = fld_->field(fid_.fid_U, ib);
             auto &old_U = fld_->field(fid_.fid_old_U, ib);
             int ncomp = U.descriptor().ncomp;
-            const Int3 &sub = U.get_lo();
-            const Int3 &sup = U.get_hi();
+            const Int3 &sub = old_U.get_lo();
+            const Int3 &sup = old_U.get_hi();
             for (int i = sub.i; i < sup.i; i++)
                 for (int j = sub.j; j < sup.j; j++)
                     for (int k = sub.k; k < sup.k; k++)
@@ -21,8 +21,8 @@ void HallMHDSolver::copy_field()
             auto &U = fld_->field(fid_.fid_Bface.xi, ib);
             auto &old_U = fld_->field(fid_.fid_old_Bface.xi, ib);
             int ncomp = U.descriptor().ncomp;
-            const Int3 &sub = U.get_lo();
-            const Int3 &sup = U.get_hi();
+            const Int3 &sub = old_U.get_lo();
+            const Int3 &sup = old_U.get_hi();
             for (int i = sub.i; i < sup.i; i++)
                 for (int j = sub.j; j < sup.j; j++)
                     for (int k = sub.k; k < sup.k; k++)
@@ -33,8 +33,8 @@ void HallMHDSolver::copy_field()
             auto &U = fld_->field(fid_.fid_Bface.eta, ib);
             auto &old_U = fld_->field(fid_.fid_old_Bface.eta, ib);
             int ncomp = U.descriptor().ncomp;
-            const Int3 &sub = U.get_lo();
-            const Int3 &sup = U.get_hi();
+            const Int3 &sub = old_U.get_lo();
+            const Int3 &sup = old_U.get_hi();
             for (int i = sub.i; i < sup.i; i++)
                 for (int j = sub.j; j < sup.j; j++)
                     for (int k = sub.k; k < sup.k; k++)
@@ -45,8 +45,8 @@ void HallMHDSolver::copy_field()
             auto &U = fld_->field(fid_.fid_Bface.zeta, ib);
             auto &old_U = fld_->field(fid_.fid_old_Bface.zeta, ib);
             int ncomp = U.descriptor().ncomp;
-            const Int3 &sub = U.get_lo();
-            const Int3 &sup = U.get_hi();
+            const Int3 &sub = old_U.get_lo();
+            const Int3 &sup = old_U.get_hi();
             for (int i = sub.i; i < sup.i; i++)
                 for (int j = sub.j; j < sup.j; j++)
                     for (int k = sub.k; k < sup.k; k++)
